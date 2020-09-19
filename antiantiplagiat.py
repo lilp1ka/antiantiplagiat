68084996b845dbc3fafea4e28e05396105520528
lilp1ka <61780263+lilp1ka@users.noreply.github.com>
2020-09-19 23:19:05 +0300

    Update antiantiplagiat.py

--- antiantiplagiat.py
+++ antiantiplagiat.py
@@ -1,6 +1,6 @@
 inputdir = input("Введите расположение своего проекта. \n Пример /storage/emulated/0/myproject/project.txt: ")
 project = open(inputdir, 'r', encoding = 'utf_8')
-replace = project.read().replace("а","a").replace("р", "p").replace("о","o").replace("х","x").replace("у","y").replace(" ","⠀")
+replace = project.read().replace(" ","⠀")
 
 outputdir = input("Введите подходящее расположение отредактированого проекта. \n /storage/emulated/0/myproject/redactedproject.txt: ")
 project2 = open(outputdir, 'w', encoding = 'utf_8')
